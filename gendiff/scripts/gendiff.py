b9010f389c2fbd57df90a5c27e257b1ee4501f1d
Sophia Filimonova <sophfil708@gmail.com>
2022-11-29 00:06:22 +0300

    add fixtures

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,11 +7,11 @@ def main():
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.')
     parser.add_argument(
-    '-f', 
-    '--format',
-    type=str,
-    default='json',
-    help='set format of output')
+        '-f',
+        '--format',
+        type=str,
+        default='json',
+        help='set format of output')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
     args = parser.parse_args()
