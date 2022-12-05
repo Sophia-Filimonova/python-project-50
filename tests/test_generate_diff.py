7a17ef8fc8c55d6909d2216a2caef46812619561
Sophia Filimonova <sophfil708@gmail.com>
2022-12-06 00:59:43 +0300

    add @pytest.mark.parametrize

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,57 +1,43 @@
 from gendiff import generate_diff
+import pytest
 
 
-def test_generate_diff_stylish_plain():
+@pytest.mark.parametrize("file1, file2, result, format", [(
+    'tests/fixtures/file1_plain.json',
+    'tests/fixtures/file2_plain.json',
+    'tests/fixtures/result_stylish_plain.txt',
+    'stylish'), (
+    'tests/fixtures/file1_plain.yaml',
+    'tests/fixtures/file2_plain.yaml',
+    'tests/fixtures/result_stylish_plain.txt',
+    'stylish'), (
+    'tests/fixtures/file1_recurs.json',
+    'tests/fixtures/file2_recurs.json',
+    'tests/fixtures/result_stylish_recurs.txt',
+    'stylish'), (
+    'tests/fixtures/file1_recurs.yaml',
+    'tests/fixtures/file2_recurs.yaml',
+    'tests/fixtures/result_stylish_recurs.txt',
+    'stylish'), (
+    'tests/fixtures/file1_recurs.json',
+    'tests/fixtures/file2_recurs.json',
+    'tests/fixtures/result_plain.txt',
+    'plain'), (
+    'tests/fixtures/file1_recurs.yaml',
+    'tests/fixtures/file2_recurs.yaml',
+    'tests/fixtures/result_plain.txt',
+    'plain'), (
+    'tests/fixtures/file1_recurs.json',
+    'tests/fixtures/file2_recurs.json',
+    'tests/fixtures/result_to_json.txt',
+    'json'), (
+    'tests/fixtures/file1_recurs.yaml',
+    'tests/fixtures/file2_recurs.yaml',
+    'tests/fixtures/result_to_json.txt',
+    'json')])
+def test_generate_diff_stylish_plain(file1, file2, result, format):
 
-    expected_result = open('tests/fixtures/result_stylish_plain.txt').read()
+    expected_result = open(result).read()
+    diff = generate_diff(file1, file2, format)
 
-    diff_json = generate_diff(
-        'tests/fixtures/file1_plain.json', 'tests/fixtures/file2_plain.json')
-    assert diff_json == expected_result
-
-    diff_yaml = generate_diff(
-        'tests/fixtures/file1_plain.yaml', 'tests/fixtures/file2_plain.yaml')
-    assert diff_yaml == expected_result
-
-
-def test_generate_diff_stylish_recursive():
-
-    expected_result = open('tests/fixtures/result_stylish_recurs.txt').read()
-
-    diff_json = generate_diff(
-        'tests/fixtures/file1_recurs.json', 'tests/fixtures/file2_recurs.json')
-    assert diff_json == expected_result
-
-    diff_yaml = generate_diff(
-        'tests/fixtures/file1_recurs.yaml', 'tests/fixtures/file2_recurs.yaml')
-    assert diff_yaml == expected_result
-
-
-def test_generate_diff_plain():
-
-    expected_result = open('tests/fixtures/result_plain.txt').read()
-
-    diff_json = generate_diff(
-        'tests/fixtures/file1_recurs.json',
-        'tests/fixtures/file2_recurs.json', 'plain')
-    assert diff_json == expected_result
-
-    diff_yaml = generate_diff(
-        'tests/fixtures/file1_recurs.yaml',
-        'tests/fixtures/file2_recurs.yaml', 'plain')
-    assert diff_yaml == expected_result
-
-
-def test_generate_diff_to_json():
-
-    expected_result = open('tests/fixtures/result_to_json.txt').read()
-
-    diff_json = generate_diff(
-        'tests/fixtures/file1_recurs.json',
-        'tests/fixtures/file2_recurs.json', 'json')
-    assert diff_json == expected_result
-
-    diff_yaml = generate_diff(
-        'tests/fixtures/file1_recurs.yaml',
-        'tests/fixtures/file2_recurs.yaml', 'json')
-    assert diff_yaml == expected_result
+    assert diff == expected_result
