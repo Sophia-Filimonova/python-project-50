3855e805959a3bd95fb1314b558875178cab5011
Sophia Filimonova <sophfil708@gmail.com>
2022-12-06 19:56:44 +0300

    update parser

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -2,10 +2,15 @@ import json
 import yaml
 
 
+def parse(file, extension):
+    if extension == 'json':
+        dict = json.load(file)
+    if extension in ('yaml', 'yml'):
+        dict = yaml.safe_load(file)
+    return dict
+
+
 def parse_file(path_to_file):
     extension = path_to_file.split('.')[-1]
-    if extension == 'json':
-        dict = json.load(open(path_to_file))
-    if extension in ('yaml', 'yml'):
-        dict = yaml.safe_load(open(path_to_file))
-    return dict
+    file = open(path_to_file)
+    return parse(file, extension)
