7fbbb8eb5655efdfd96cd822b7530b0007816af9
Sophia Filimonova <sophfil708@gmail.com>
2022-12-08 23:34:11 +0300

    update formaters

--- gendiff/formaters/plain.py
+++ gendiff/formaters/plain.py
@@ -8,29 +8,25 @@ def stringify(value):
     return str(value).lower()
 
 
-def plain(diff_tree):
-
-    def iter_(tree, path=[]):
-        lines = []
-        for node in tree:
-            path.append(node["key"])
-            full_path = '.'.join(path)
-            if node["action"] == "added":
-                lines.append(
-                    f"Property '{full_path}' was added "
-                    f'with value: {stringify(node["value1"])}')
-            elif node["action"] == "removed":
-                lines.append(
-                    f"Property '{full_path}' was removed")
-            elif node["action"] == "changed":
-                lines.append(
-                    f"Property '{full_path}' was updated. "
-                    f'From {stringify(node["value1"])} '
-                    f'to {stringify(node["value2"])}')
-            elif node["action"] == "nested":
-                lines.append(iter_(node["children"], path))
-            path.pop()
-        output = '\n'.join(lines)
-        return output
-
-    return iter_(diff_tree)
+def plain(tree, path=[]):
+    lines = []
+    for node in tree:
+        path.append(node["key"])
+        full_path = '.'.join(path)
+        if node["action"] == "added":
+            lines.append(
+                f"Property '{full_path}' was added "
+                f'with value: {stringify(node["value1"])}')
+        elif node["action"] == "removed":
+            lines.append(
+                f"Property '{full_path}' was removed")
+        elif node["action"] == "changed":
+            lines.append(
+                f"Property '{full_path}' was updated. "
+                f'From {stringify(node["value1"])} '
+                f'to {stringify(node["value2"])}')
+        elif node["action"] == "nested":
+            lines.append(plain(node["children"], path))
+        path.pop()
+    output = '\n'.join(lines)
+    return output
--- gendiff/formaters/stylish.py
+++ gendiff/formaters/stylish.py
@@ -1,4 +1,6 @@
-def stringify(value, indent):
+def convert(value, indent):
+    if value == "no such key":
+        return None
     if value is None:
         return 'null'
     if isinstance(value, str):
@@ -8,43 +10,30 @@ def stringify(value, indent):
         indent += '    '
         lines = ['{']
         for key in keys:
-            lines.append(f"{indent}    {key}: {stringify(value[key], indent)}")
+            lines.append(f"{indent}    {key}: {convert(value[key], indent)}")
         lines.append(indent + '}')
         return '\n'.join(lines)
     return str(value).lower()
 
 
-def stylish(diff_tree):
-
-    def iter_(tree, depth=0):
-        indent = '    ' * depth
-        lines = ['{']
-        for node in tree:
-            if node["action"] == "added":
-                lines.append(
-                    f'{indent}  + {node["key"]}: '
-                    f'{stringify(node["value1"], indent)}')
-            elif node["action"] == "removed":
-                lines.append(
-                    f'{indent}  - {node["key"]}: '
-                    f'{stringify(node["value1"], indent)}')
-            elif node["action"] == "same":
-                lines.append(
-                    f'{indent}    {node["key"]}: '
-                    f'{stringify(node["value1"], indent)}')
-            elif node["action"] == "changed":
-                lines.append(
-                    f'{indent}  - {node["key"]}: '
-                    f'{stringify(node["value1"], indent)}')
-                lines.append(
-                    f'{indent}  + {node["key"]}: '
-                    f'{stringify(node["value2"], indent)}')
-            elif node["action"] == "nested":
-                lines.append(
-                    f'{indent}    {node["key"]}: '
-                    f'{iter_(node["children"], depth + 1)}')
-        lines.append(indent + '}')
-        output = '\n'.join(lines)
-        return output
-
-    return iter_(diff_tree)
+def stylish(tree, depth=0):   # noqa: C901
+    indent = '    ' * depth
+    lines = ['{']
+    for node in tree:
+        key = node["key"]
+        value1 = convert(node.get("value1", "no such key"), indent)
+        value2 = convert(node.get("value2", "no such key"), indent)
+        if node["action"] == "nested":
+            lines.append(f'{indent}    {key}: '
+                         f'{stylish(node["children"], depth + 1)}')
+        elif node["action"] == "added":
+            lines.append(f'{indent}  + {key}: {value1}')
+        elif node["action"] == "removed":
+            lines.append(f'{indent}  - {key}: {value1}')
+        elif node["action"] == "same":
+            lines.append(f'{indent}    {key}: {value1}')
+        elif node["action"] == "changed":
+            lines.append(f'{indent}  - {key}: {value1}')
+            lines.append(f'{indent}  + {key}: {value2}')
+    lines.append(indent + '}')
+    return '\n'.join(lines)
